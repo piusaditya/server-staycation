6c422242ba6581612b7ba664f1c590bae2d734b4
piusaditya <36447496+piusaditya@users.noreply.github.com>
2021-02-26 23:30:17 +0700

    deploy heroku

--- app.js
+++ app.js
@@ -8,12 +8,15 @@ const session = require("express-session");
 const flash = require("connect-flash");
 // import mongoose
 const mongoose = require("mongoose");
-mongoose.connect("mongodb://127.0.0.1:27017/db_staycation", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
-});
+mongoose.connect(
+  "mongodb+srv://piusaditya:E2VUX2QY@mUaXVL@cluster0.jojuc.mongodb.net/db_staycation?retryWrites=true&w=majority",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  }
+);
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
